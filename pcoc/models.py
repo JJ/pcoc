2ffb5ed0315407b60820e827196d4c13f2c33636
migue <migue@nano.(none)>
2008-10-09 00:31:06 +0200

    bad merge

--- pcoc/models.py
+++ pcoc/models.py
@@ -54,12 +54,6 @@ def init_ga_params(force=False):
 
     return "<br>\n".join(out)
 
-    return "\n".join(out)
-            
-
-    return "\n".join(out)
-            
-
 def get_param(name):
     return Param.all().filter("name =",name).get()
 
